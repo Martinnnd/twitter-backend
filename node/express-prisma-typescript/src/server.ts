7d9de0708e21ec8122d26af873c794700dc00f6b
Martin de Oro <deoromartinignacio@gmail.com>
2025-03-18 11:43:31 -0300

    changes

--- node/express-prisma-typescript/src/server.ts
+++ node/express-prisma-typescript/src/server.ts
@@ -25,7 +25,7 @@ app.use(cookieParser()) // Parse cookies
 
 app.use(
   cors({
-    origin: Constants.CORS_WHITELIST,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
